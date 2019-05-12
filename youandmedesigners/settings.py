eaf23edd04daccefed56dc337a1e2aa574612a93
Manoj Kashyap <manojrkashyap@outlook.com>
2019-05-12 13:26:16 +0530

    changes

--- youandmedesigners/settings.py
+++ youandmedesigners/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7^34ohsguqbul4c__!-5fgl5l^p%cr!qx_d@!k)b5yh1w%+fuf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
